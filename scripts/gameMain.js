d3c64713d0fedc318537a72d3eb85ae5291488ca
efrat-burshteyn <efrat4687@gmail.com>
2026-04-28 22:17:44 +0300

    פונקצית אתחול של המשחק ועוד קצת הסברים

--- scripts/gameMain.js
+++ scripts/gameMain.js
@@ -1,2 +1,19 @@
+/**
+ * @fileoverview פונקציות להצגת המשחק.
+ * @author [Efrat Burshteyn]
+ */
+/**יבוא הפונקציות */
 import { createDeck, shuffle, drawCard } from './gameLogic.js';
+
+let deck=[];
+let usedCards=[];
+/**
+ * פונקציית האתחול של המשחק.
+ * יוצרת את החפיסה, מערבבת אותה ומכינה את המשתנים לתחילת המשחק.
+ */
+const initGame=()=>{
+    deck=createDeck();
+    shuffle(deck);
+}
+
 
