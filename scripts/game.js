5cc5d42bf20b94824558660d0a92fca3ff54ecc1
efrat-burshteyn <efrat4687@gmail.com>
2026-04-28 20:07:04 +0300

    פונקציה שיצרת את ערימת קלפי המשחק

---
+++ scripts/game.js
@@ -0,0 +1,27 @@
+ /**מערך סוגי הקלפים */
+const ingredients=['chips','falafel','humus','pickles','pita','salad','tahini','broken pita'];
+ /**מערך קלפי המשחק */
+const deck=[];
+/**
+ * יוצרת את חפיסת הקלפים למשחק.
+ * הפונקציה ממלאת את המערך ב-20 עותקים מכל מרכיב תקין,
+ * ומוסיפה "פיתות קרועות" בהתאם לרמת הקושי שנשמרה בזיכרון.
+ * * @returns {void} הפונקציה מעדכנת את המערך הגלובלי deck.
+ */
+export const createDeck = () =>{
+    let k=0,t=7;
+    const level=sessionStorage.getItem('selectedLevel');
+  for(let i=0; i<20; i++){
+        for(let j=0; j<7; j++ ){
+        deck[k++]=ingredients[j];
+    }
+}
+    if(level==='easy'){
+    for(let i=0;i<8;i++)
+        deck[k++]=ingredients[t];
+    }
+    else{
+      for(let i=0;i<16;i++)
+        deck[k++]=ingredients[t];  
+    }
+}
