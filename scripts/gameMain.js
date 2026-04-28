9256589da18b982180a71f7738cdcdd38d659476
efrat-burshteyn <efrat4687@gmail.com>
2026-04-28 23:28:45 +0300

    עדכון פונקצית האתחול - תוספת יצירת חפיסת קלפי המלכים ועדכום השחקן הראשון כפעיל

--- scripts/gameMain.js
+++ scripts/gameMain.js
@@ -3,17 +3,53 @@
  * @author [Efrat Burshteyn]
  */
 /**יבוא הפונקציות */
-import { createDeck, shuffle, drawCard } from './gameLogic.js';
+import { createDeck, shuffle, drawCard,createKindsDeck } from './gameLogic.js';
 
 let deck=[];
 let usedCards=[];
+let kindsCards=[];
+let card;
+let player=0;
 /**
  * פונקציית האתחול של המשחק.
- * יוצרת את החפיסה, מערבבת אותה ומכינה את המשתנים לתחילת המשחק.
+ * יוצרת את החפיסת הקלפים, מערבבת אותה ויוצרת את חפיסת קלפי המלכים ומכינה את המשתנים לתחילת המשחק.
+  *ועדכון השחקן הראשון כפעיל  
  */
 const initGame=()=>{
     deck=createDeck();
     shuffle(deck);
+    kindsCards=createKindsDeck();
+    player=0;
 }
+/**אוביקט של מערכי לוחות השחחקנים לכל שחקן דוכם רכיבין ומערך לכרטיסי המלך */
+let playersBoards=[{ingredients:[],kinds:[]},{ingredients:[],kinds:[]}];
 
+/**
+ * מטפלת בלוגיקה של שליפת קלף מהקופה ועדכון לוח השחקן.
+ * הפונקציה בודקת שלושה מצבים:
+ * 1. פיתה קרועה: איפוס הדוכן של השחקן הנוכחי.
+ * 2. כפילות: העברת הקלף לערימת המשומשים.
+ * 3. רכיב חדש: הוספה לדוכן ובדיקת השלמת מנה (7 רכיבים) לקבלת מלך.
+ */
+const handleDraw=()=>{
+   card=drawCard()
+   if(card==='broken pita')
+       clearBoard(playersBoards[player].ingredients);
+    else if(playersBoards[player].ingredients.includes(card))
+              usedCards.push(card);
+    else{
+         playersBoards[player].ingredients.push(card);
+         if(playersBoards[player].ingredients.length===7){
+             playersBoards[player].kinds.push(kindsCards.pop());
+             clearBoard(playersBoards[player].ingredients);
+         }
+    }
+    switchTurn();
+ }
+ /**
+ * פונקציה להחלפת תורות בין השחקנים.
+ * הפונקציה מעדכנת את המשתנה הלוגי player ומחליפה את העיצוב הוויזואלי
+ * כדי לסמן לשחקנים מי השחקן הפעיל כעת.
+ */
+ 
 
